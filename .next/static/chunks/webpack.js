b88fe65aa4334accc5dace83e8a8b0ee4c2ef7ef
NicholasGomis <nicholasgomis@icloud.com>
2022-02-19 00:02:39 +0000

    Added projects titles paragraph and image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f45a85faefa790e"; }
+/******/ 		__webpack_require__.h = function() { return "9576c208f10cb270"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/projects.js
+++ pages/projects.js
@@ -7,8 +7,15 @@ export default function Projects(props) {
         
         <div> 
         
-        <h1>This is the projects page</h1>
-        
+        <h1> 4 week national project </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
+        <img src={"https://s3.amazonaws.com/creativetim_bucket/products/500/original/soft-ui-dashboard-material-ui.jpg?1632195046"} alt="Logo" />;
+
+         <h1> 1 week national project </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
+
+        <h1>Christmas project </h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
         </div>
     )
 }
