40ba55bc8c5b8ec3c4d14c96381e2e6697089037
NicholasGomis <nicholasgomis@icloud.com>
2022-02-20 14:36:17 +0000

    added app names  and tags

--- .next/static/webpack/webpack.a8989a29f28b362e.hot-update.js
+++ .next/static/webpack/webpack.a8989a29f28b362e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ee34ad2a4c17613c"; }
+/******/ 	__webpack_require__.h = function() { return "44977204e52c1428"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Projects.module.css'
 export default function Projects(props) {
     
 
@@ -8,10 +8,10 @@ export default function Projects(props) {
         
         <div> 
         
-    <h1> Health and wellbeing app </h1>
+    <h1> Heawell - Health and wellbeing app </h1>
 
     <a className={styles.tags}> #React.js </a>
-     <a className={styles.tags}> #CSS </a>
+    <a className={styles.tags}> #CSS </a>
     <p className={styles.tags}> #Express.js </p>
     <p className={styles.tags}> #Postgresql </p>
     <p className={styles.tags}> #Jest </p>
@@ -22,10 +22,23 @@ export default function Projects(props) {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
         <img src={"https://s3.amazonaws.com/creativetim_bucket/products/500/original/soft-ui-dashboard-material-ui.jpg?1632195046"} alt="Logo" />;
 
-         <h1> 1 week national project </h1>
+         <h1> Code Buddy -  question and answer for developers </h1>
+        <a className={styles.tags}> #React.js </a>
+        <a className={styles.tags}> #CSS </a>
+        <p className={styles.tags}> #Express.js </p>
+        <p className={styles.tags}> #Postgresql </p>
+        <p className={styles.tags}> #Jest </p>
+    
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
 
-        <h1>Christmas project </h1>
+        <h1> Who has seen it - Track missing people </h1>
+
+     <a className={styles.tags}> #React.js </a>
+    <a className={styles.tags}> #CSS </a>
+    <p className={styles.tags}> #Express.js </p>
+    <p className={styles.tags}> #Postgresql </p>
+    <p className={styles.tags}> #Jest </p>
+    
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet leo vitae vulputate consectetur. Donec eget semper nunc. Vestibulum non nunc in sem hendrerit rutrum nec sed risus. Aenean et eleifend ante</p>
         </div>
     )
