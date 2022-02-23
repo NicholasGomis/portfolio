3c0caba48bfc0358b79a593a6b923e9e6445b7b7
NicholasGomis <nicholasgomis@icloud.com>
2022-02-23 18:51:54 +0000

    Removed hover background added hover colour for elements

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "598b2022f7c6e4d1"; }
+/******/ 		__webpack_require__.h = function() { return "6bb6f328b7719121"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
