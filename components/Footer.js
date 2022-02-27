8404810c16d1c4db7339759862ed3cc0c76a9c37
NicholasGomis <nicholasgomis@icloud.com>
2022-02-27 18:09:34 +0000

    coorected Footer to footer Vercel error

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 
 
-export function Footer() {
+export function footer() {
     
 
     return (
--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export function Layout({children}) {
 <div className="content">
 <Navbar />
 {children}
-<Footer />
+<footer />
 
 </div>
     )
