b9c9d66eaaf0ef690a97bc19b1d2a11ac7f646e8
NicholasGomis <nicholasgomis@icloud.com>
2022-02-28 19:26:20 +0000

    Alternative download button direct to google drive rendered it

--- pages/project1/project1.js
+++ pages/project1/project1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-
+import {HiDownload} from "react-icons/hi"
 import styles from '../../styles/Projects.module.css'
 
 
@@ -13,10 +13,19 @@ export default function Projectone(props) {
     <Link href={"/projects"}>
     <a className={styles.btntwo}> ← Projects </a>
     </Link>,
-     <h1>Heawell - Health and wellbeing app</h1>
+     <h1>Heawell - Health and wellbeing app </h1>
+       
+     <h2> Key tools </h2>
+
+
+      <Link href={"https://drive.google.com/drive/folders/1-QgsxznauxlYLIX3zSxsqTsvxKhEckAI?usp=sharing"}>
+    <a className={styles.btntwo}> <HiDownload /> Download </a>
+    </Link>,
+
+
     </div>
 
-      
+     
 
     )
     
