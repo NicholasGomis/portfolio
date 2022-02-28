71a5fe252c3798b1abf915371712d975e64657b7
NicholasGomis <nicholasgomis@icloud.com>
2022-02-28 20:24:57 +0000

    Added list of key tools in project one

--- pages/project1/project1.js
+++ pages/project1/project1.js
@@ -12,20 +12,30 @@ export default function Projectone(props) {
 <div className='testing'>
     <Link href={"/projects"}>
     <a className={styles.btntwo}> ← Projects </a>
-    </Link>,
-     <h1>Heawell - Health and wellbeing app </h1>
-       
-     <h2> Key tools </h2>
+    </Link>
+    <h1>Heawell - Health and wellbeing app </h1>
+    
 
 
       <Link href={"https://drive.google.com/drive/folders/1-QgsxznauxlYLIX3zSxsqTsvxKhEckAI?usp=sharing"}>
     <a className={styles.btntwo}> <HiDownload /> Download </a>
-    </Link>,
+    </Link>
 
+   <h2> Key tools </h2>
+<ul>
+    <li>Kanban map</li>
+    <li>User stories</li>
+    <li>Wireframes (high low fidelity)</li>
+    <li>Version control</li>
+    <li>User Journey (UX/UI)</li>
+    <li>Quantity & Quality data </li>
+    <li> User personas </li>
+   
+</ul>
 
     </div>
 
-     
+    
 
     )
     
