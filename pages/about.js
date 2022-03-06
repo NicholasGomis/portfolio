02bd176936f7ce87710da526de6fa1c205969ff1
NicholasGomis <nicholasgomis@icloud.com>
2022-03-06 14:46:10 +0000

    Fixed vercel deployed failure

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function About(props) {
             
             <h1> 👋 Meet Nick </h1>
             
-            <p> Hello everyone! I am Nicholas. I was born and raised in Italy. Throughout my childhood I always have been fascinated with technology. During my afternoons, rather than playing football with friends, I used to buy broken computers to learn more about it's components and build my own machine. this new hobby led me to study Electrical and Electronic in High School.
+            <p> Hello everyone! I am Nicholas. I was born and raised in Italy. Throughout my childhood I always have been fascinated with technology. During my afternoons, rather than playing football with friends, I used to buy broken computers to learn more about it's components and build my own machine. this new hobby led me to study Electrical and Electronic in High School..
             </p>
            
             <p> Once I earned my diploma, I decided to continue my academic studies in the United Kingdom, due to its opportunities and the diversities which had always intrigued me. During my time in the UK, I gained my Bachelor in Computer Hardware and Software Engineer at Coventry University and a MSc in Manufacturing Engineering with Management at University of Warwick.</p>
