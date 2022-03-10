579a756bbe548e3d44cec2915943f68e083902cf
NicholasGomis <nicholasgomis@icloud.com>
2022-03-10 23:14:27 +0000

    Cards top level started

--- components/Cards.js
+++ components/Cards.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import css from  './About.module.css'
 
 export function Cards(props) {
     
 
     return (
-       <div className='cardBlock'>
-        <h1> This is a card.</h1>
+       <div className='card-container'>
+{/* <div className='image-container'>
+        <img src = {imageURL} alt=''/>
+</div> */}
+    <h1> This is a card </h1>
        </div>
+
+   
     )
 }
